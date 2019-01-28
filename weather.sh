fc88108bc709b834a53d28bf3ec82dd7a00ed30f
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2019-01-28 14:40:40 +0100

    Added icons depending on weather Added color change depending on temperature

--- weather.sh
+++ weather.sh
@@ -4,21 +4,38 @@ CITY=""
 API_KEY=""
 UNIT="metric"
 
-SUNNY=""       # https://fontawesome.com/icons/sun?style=solid
-SUN_CLOUD=""   # https://fontawesome.com/icons/cloud-sun?style=solid
+TMP_FILE=$(mktemp)
 
-TEMP_C=$(curl "http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=${UNIT}" 2>/dev/null | jq '.main.temp')
+curl "http://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=${UNIT}" 2>/dev/null > $TMP_FILE
+
+TEMP_C=$(cat $TMP_FILE | jq '.main.temp')
+TEMP_INT=$(echo "scale=0; $TEMP_C/1" | bc)
+
+WEATHER=$(cat $TMP_FILE | jq '.weather[-1].main' -r)
 
 RED=$(xrdb_q color1)
 ORANGE=$(xrdb_q color3)
 BLUE=$(xrdb_q color4)
+
 case "$WEATHER" in
-    "clear sky")
-        WEATHER=""
-        ;;
+    "Sunny")
+        WEATHER_ICON="";;      # https://fontawesome.com/icons/sun?style=solid
+    "Rain")
+        WEATHER_ICON="";;     # https://fontawesome.com/icons/sun?style=solid
+    "Snow")
+        WEATHER_ICON="";;
     *)
-        WEATHER=""
+        WEATHER_ICON=$WEATHER;;
 esac
 
-echo "$WEATHER %{F#$BLUE}$TEMP_C°C"
+if (( TEMP_INT > 15 )); then
+    TEMP_COLOR=${RED}
+elif (( TEMP_INT > -5 && TEMP_INT < 15 )); then
+    TEMP_COLOR=${ORANGE}
+else
+    TEMP_COLOR=${BLUE}
+fi
 
+echo "$WEATHER_ICON %{F#$BLUE}$TEMP_C°C"
+
+rm $TMP_FILE
