7b965afe47110a771dd4c6a78dfd9e38ac89fe72
Jon Erling Hustadnes <JonErling.Hustadnes@gmail.com>
2019-02-13 20:11:06 +0100

    Given icons to more weather states
    Added more comments
    Adjusted temperature ranges.
    Added green color

--- weather.sh
+++ weather.sh
@@ -13,34 +13,37 @@ TEMP_INT=$(echo "scale=0; $TEMPERATURE/1" | bc 2>/dev/null)
 
 WEATHER=$(cat $TMP_FILE | jq '.weather[-1].main' -r)
 
+rm $TMP_FILE
+
 RED=$(xrdb_q color1)
+GREEN=$(xrdb_q color2)
 ORANGE=$(xrdb_q color3)
 BLUE=$(xrdb_q color4)
 
 case "$WEATHER" in
-    "Sunny")
-        WEATHER_ICON="";;  # https://fontawesome.com/icons/sun?style=solid
-    "Rain")
-        WEATHER_ICON="";;  # https://fontawesome.com/icons/sun?style=solid
+    "Sunny"|"Clear")
+        WEATHER_ICON="";;              # https://fontawesome.com/icons/sun?style=solid
+    "Rain"|"Drizzle")
+        WEATHER_ICON="";;              # https://fontawesome.com/icons/cloud-rain?style=solid
     "Snow")
-        WEATHER_ICON="";;
+        WEATHER_ICON="";;              # https://fontawesome.com/icons/snowflake?style=solid
     "Clouds")
-        WEATHER_ICON="";;
-    "")
-        WEATHER_ICON="%{F#$RED}";;
-    *)
+        WEATHER_ICON="";;              # https://fontawesome.com/icons/cloud?style=solid
+    "") # In case of errors
+        WEATHER_ICON="%{F#$RED}";;     # https://fontawesome.com/icons/poo-storm?style=solid
+    *)  # Fallback to text
         WEATHER_ICON=$WEATHER;;
 esac
 
 if (( TEMP_INT > 20 )); then
     TEMP_COLOR=${RED}
-elif (( TEMP_INT > 5 && TEMP_INT < 20 )); then
+elif (( TEMP_INT >= 10 && TEMP_INT < 20 )); then
     TEMP_COLOR=${ORANGE}
+elif (( TEMP_INT >= 5 && TEMP_INT < 10 )); then
+    TEMP_COLOR=${GREEN}
 elif (( TEMP_INT < 5 )); then
     TEMP_COLOR=${BLUE}
 fi
 
 [[ ! $TEMPERATURE ]] && TEMPERATURE="--"
 echo "$WEATHER_ICON %{F#$TEMP_COLOR}$TEMPERATURE°C"
-
-rm $TMP_FILE
